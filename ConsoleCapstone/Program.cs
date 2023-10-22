51c971835fead8be4fca9638bbaf2f2a393fd261
carltoncm <carltoncm22@gmail.com>
2023-10-22 13:21:33 -0500

    Adding Callie's games

--- ConsoleCapstone/Program.cs
+++ ConsoleCapstone/Program.cs
@@ -72,8 +72,9 @@ while (playingTheGame)
 	"5: Neebles's Game",
 	"6: Rachel's Game",
 	"7: Aaron's Game",
-	"8: Cali's Game",
-	"9: Thermonuclear War"
+	"8: Tic Tac Toe",
+	"9: Thermonuclear War",
+    "10: Whack-A-Nerd"
 };
 
 	//list the tricks
@@ -119,12 +120,17 @@ while (playingTheGame)
 			break;
 		case "8":
 			selectedGame = SelectedGame.Cali;
-			break;
+			PlayTicTacToe();
+            break;
 		case "9":
 			selectedGame = SelectedGame.War;
 			PlayWar();
 			break;
-		default:
+        case "10":
+            selectedGame = SelectedGame.Cali2;
+            PlayWhackANerd();
+            break;
+        default:
 			break;
 	}
 
@@ -272,6 +278,311 @@ void PlayBigWalletGangstas()
 	}
 }
 
+void PlayTicTacToe()
+{
+    bool closeRequested = false;
+    bool playerTurn = true;
+    Random random = new();
+    char[,] board;
+
+    while (!closeRequested)
+    {
+        board = new char[3, 3]
+        {
+        { ' ', ' ', ' ', },
+        { ' ', ' ', ' ', },
+        { ' ', ' ', ' ', },
+        };
+        while (!closeRequested)
+        {
+            if (playerTurn)
+            {
+                PlayerTurn();
+                if (CheckForThree('X'))
+                {
+                    EndGame("You Win.");
+                    break;
+                }
+            }
+            else
+            {
+                ComputerTurn();
+                if (CheckForThree('O'))
+                {
+                    EndGame("You Lose.");
+                    break;
+                }
+            }
+            playerTurn = !playerTurn;
+            if (CheckForFullBoard())
+            {
+                EndGame("Draw.");
+                break;
+            }
+        }
+        if (!closeRequested)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Play Again [enter], or quit [escape]?");
+        GetInput:
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.Enter: break;
+                case ConsoleKey.Escape:
+                    closeRequested = true;
+                    Console.Clear();
+                    break;
+                default: goto GetInput;
+            }
+        }
+    }
+
+    void PlayerTurn()
+    {
+        var (row, column) = (0, 0);
+        bool moved = false;
+        while (!moved && !closeRequested)
+        {
+            Console.Clear();
+            RenderBoard();
+            Console.WriteLine();
+            Console.WriteLine("Choose a valid position and press enter.");
+            Console.SetCursorPosition(column * 6 + 1, row * 4 + 1);
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.UpArrow: row = row <= 0 ? 2 : row - 1; break;
+                case ConsoleKey.DownArrow: row = row >= 2 ? 0 : row + 1; break;
+                case ConsoleKey.LeftArrow: column = column <= 0 ? 2 : column - 1; break;
+                case ConsoleKey.RightArrow: column = column >= 2 ? 0 : column + 1; break;
+                case ConsoleKey.Enter:
+                    if (board[row, column] is ' ')
+                    {
+                        board[row, column] = 'X';
+                        moved = true;
+                    }
+                    break;
+                case ConsoleKey.Escape:
+                    Console.Clear();
+                    closeRequested = true;
+                    break;
+            }
+        }
+    }
+
+    void ComputerTurn()
+    {
+        var possibleMoves = new List<(int X, int Y)>();
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (board[i, j] == ' ')
+                {
+                    possibleMoves.Add((i, j));
+                }
+            }
+        }
+        int index = random.Next(0, possibleMoves.Count);
+        var (X, Y) = possibleMoves[index];
+        board[X, Y] = 'O';
+    }
+
+    bool CheckForThree(char c) =>
+        board[0, 0] == c && board[1, 0] == c && board[2, 0] == c ||
+        board[0, 1] == c && board[1, 1] == c && board[2, 1] == c ||
+        board[0, 2] == c && board[1, 2] == c && board[2, 2] == c ||
+        board[0, 0] == c && board[0, 1] == c && board[0, 2] == c ||
+        board[1, 0] == c && board[1, 1] == c && board[1, 2] == c ||
+        board[2, 0] == c && board[2, 1] == c && board[2, 2] == c ||
+        board[0, 0] == c && board[1, 1] == c && board[2, 2] == c ||
+        board[2, 0] == c && board[1, 1] == c && board[0, 2] == c;
+
+    bool CheckForFullBoard() =>
+        board[0, 0] != ' ' && board[1, 0] != ' ' && board[2, 0] != ' ' &&
+        board[0, 1] != ' ' && board[1, 1] != ' ' && board[2, 1] != ' ' &&
+        board[0, 2] != ' ' && board[1, 2] != ' ' && board[2, 2] != ' ';
+
+    void RenderBoard()
+    {
+        Console.WriteLine();
+        Console.WriteLine($" {board[0, 0]}  ║  {board[0, 1]}  ║  {board[0, 2]}");
+        Console.WriteLine("    ║     ║");
+        Console.WriteLine(" ═══╬═════╬═══");
+        Console.WriteLine("    ║     ║");
+        Console.WriteLine($" {board[1, 0]}  ║  {board[1, 1]}  ║  {board[1, 2]}");
+        Console.WriteLine("    ║     ║");
+        Console.WriteLine(" ═══╬═════╬═══");
+        Console.WriteLine("    ║     ║");
+        Console.WriteLine($" {board[2, 0]}  ║  {board[2, 1]}  ║  {board[2, 2]}");
+    }
+
+    void EndGame(string message)
+    {
+        Console.Clear();
+        RenderBoard();
+        Console.WriteLine();
+        Console.Write(message);
+    }
+}
+
+void PlayWhackANerd()
+{
+    string Board =
+    @" ╔═══╦═══════╗ ╔═══╦═══════╗ ╔═══╦═══════╗" + '\n' +
+    @" ║ 7 ║       ║ ║ 8 ║       ║ ║ 9 ║       ║" + '\n' +
+    @" ╚═══╣       ║ ╚═══╣       ║ ╚═══╣       ║" + '\n' +
+    @"     ║       ║     ║       ║     ║       ║" + '\n' +
+    @"     ║       ║     ║       ║     ║       ║" + '\n' +
+    @"     ╚═══════╝     ╚═══════╝     ╚═══════╝" + '\n' +
+    @" ╔═══╦═══════╗ ╔═══╦═══════╗ ╔═══╦═══════╗" + '\n' +
+    @" ║ 4 ║       ║ ║ 5 ║       ║ ║ 6 ║       ║" + '\n' +
+    @" ╚═══╣       ║ ╚═══╣       ║ ╚═══╣       ║" + '\n' +
+    @"     ║       ║     ║       ║     ║       ║" + '\n' +
+    @"     ║       ║     ║       ║     ║       ║" + '\n' +
+    @"     ╚═══════╝     ╚═══════╝     ╚═══════╝" + '\n' +
+    @" ╔═══╦═══════╗ ╔═══╦═══════╗ ╔═══╦═══════╗" + '\n' +
+    @" ║ 1 ║       ║ ║ 2 ║       ║ ║ 3 ║       ║" + '\n' +
+    @" ╚═══╣       ║ ╚═══╣       ║ ╚═══╣       ║" + '\n' +
+    @"     ║       ║     ║       ║     ║       ║" + '\n' +
+    @"     ║       ║     ║       ║     ║       ║" + '\n' +
+    @"     ╚═══════╝     ╚═══════╝     ╚═══════╝";
+
+    string Mole =
+        @" ╔══─┐ " + '\n' +
+        @" │o-o│ " + '\n' +
+        @"┌└───┘┐" + '\n' +
+        @"││ N ││";
+
+    string Empty =
+        @"       " + '\n' +
+        @"       " + '\n' +
+        @"       " + '\n' +
+        @"       ";
+
+    TimeSpan playTime = TimeSpan.FromSeconds(30);
+
+    if (OperatingSystem.IsWindows())
+    {
+        Console.WindowWidth = Math.Max(Console.WindowWidth, 50);
+        Console.WindowHeight = Math.Max(Console.WindowHeight, 22);
+    }
+
+    while (true)
+    {
+        Console.Clear();
+        Console.WriteLine("Whack-A-Nerd");
+        Console.WriteLine();
+        Console.WriteLine(
+            $"You have {(int)playTime.TotalSeconds} seconds to whack as many nerds as you " +
+            "can before the timer runs out. Use the number keys 1-9 to whack. Are you ready? ");
+        Console.WriteLine();
+        Console.WriteLine("Play [enter], or quit [escape]?");
+    GetInput:
+        switch (Console.ReadKey(true).Key)
+        {
+            case ConsoleKey.Enter:
+                Play();
+                break;
+            case ConsoleKey.Escape:
+                Console.Clear();
+                Console.WriteLine("Whack-A-Nerd was closed...");
+                Environment.Exit(0);
+                break;
+            default: goto GetInput;
+        }
+    }
+
+    void Play()
+    {
+        Console.Clear();
+        Console.WriteLine("Whack-A-Nerd");
+        Console.WriteLine();
+        Console.WriteLine(Board);
+        DateTime start = DateTime.Now;
+        int score = 0;
+        Random random = new();
+        int moleLocation = random.Next(1, 10);
+        Console.CursorVisible = false;
+        while (DateTime.Now - start < playTime)
+        {
+            var (left, top) = Map(moleLocation);
+            Console.SetCursorPosition(left, top);
+            Render(Mole);
+            int selection;
+        GetInput:
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.D1: case ConsoleKey.NumPad1: selection = 1; break;
+                case ConsoleKey.D2: case ConsoleKey.NumPad2: selection = 2; break;
+                case ConsoleKey.D3: case ConsoleKey.NumPad3: selection = 3; break;
+                case ConsoleKey.D4: case ConsoleKey.NumPad4: selection = 4; break;
+                case ConsoleKey.D5: case ConsoleKey.NumPad5: selection = 5; break;
+                case ConsoleKey.D6: case ConsoleKey.NumPad6: selection = 6; break;
+                case ConsoleKey.D7: case ConsoleKey.NumPad7: selection = 7; break;
+                case ConsoleKey.D8: case ConsoleKey.NumPad8: selection = 8; break;
+                case ConsoleKey.D9: case ConsoleKey.NumPad9: selection = 9; break;
+                case ConsoleKey.Escape:
+                    Console.Clear();
+                    Console.WriteLine("Whack-A-Nerd was closed...");
+                    Environment.Exit(0);
+                    return;
+                default: goto GetInput;
+            }
+            if (moleLocation == selection)
+            {
+                score++;
+                Console.SetCursorPosition(left, top);
+                Render(Empty);
+                int newMoleLocation = random.Next(1, 9);
+                moleLocation = newMoleLocation >= moleLocation ? newMoleLocation + 1 : newMoleLocation;
+            }
+        }
+        Console.CursorVisible = true;
+        Console.Clear();
+        Console.WriteLine("Whack-A-Nerd");
+        Console.WriteLine();
+        Console.WriteLine(Board);
+        Console.WriteLine();
+        Console.WriteLine("Game Over. Score: " + score);
+        Console.WriteLine("Hopefully those nerds will learn their lesson and find a girlfriend!");
+        Console.WriteLine();
+        Console.WriteLine("Press [Enter] To Continue...");
+        Console.ReadLine();
+    }
+
+    (int Left, int Top) Map(int hole) =>
+        hole switch
+        {
+            1 => (06, 15),
+            2 => (20, 15),
+            3 => (34, 15),
+            4 => (06, 09),
+            5 => (20, 09),
+            6 => (34, 09),
+            7 => (06, 03),
+            8 => (20, 03),
+            9 => (34, 03),
+            _ => throw new NotImplementedException(),
+        };
+
+    void Render(string @string)
+    {
+        int x = Console.CursorLeft;
+        int y = Console.CursorTop;
+        foreach (char c in @string)
+        {
+            if (c is '\n')
+            {
+                Console.SetCursorPosition(x, ++y);
+            }
+            else
+            {
+                Console.Write(c);
+            }
+        }
+    }
+}
+
 void WaitForEnter()
 {
     while (Console.ReadKey(intercept: true).Key != ConsoleKey.Enter) ;
@@ -381,5 +692,6 @@ public enum SelectedGame
 	Rachel,
 	Cali,
 	Aaron,
-	War
+	War,
+    Cali2
 }
